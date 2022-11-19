55c5430873d3ced8444ac6710a489b3409b06328
moon34git <moon34git@gmail.com>
2022-11-19 20:06:13 +0900

    Processing #2 and Anomaly detection with Isolation Forest

--- main.py
+++ main.py
@@ -18,33 +18,33 @@ for i in range(len(flights)):
 
 flights = flights.drop(index)
 
-path1 = ['JF','LA']
-path2 = ['FL', 'AT']
-path3 = ['OR','DF']
+path1 = ['JFK','LAX']
+path2 = ['FLL', 'ATL']
+path3 = ['ORD','DFW']
 
-path4 = ['BO','DC']
-path5 = ['DE', 'LA']
-path6 = ['SE','LA']
-path7 = ['SF','LA']
-path8 = ['MS','OR']
+path4 = ['BOS','DCA']
+path5 = ['DEN', 'LAX']
+path6 = ['SEA','LAX']
+path7 = ['SFO','LAX']
+path8 = ['MSP','ORD']
 
 d_path = path1 + path2 + path3
 o_path = path4 + path5 + path6 + path7 + path8
 
 flights_v2 = pd.DataFrame([])
 for i in range(0,10,2):
-    temp_df1 = flights.loc[(flights['ORIGIN_AIRPORT'] == o_path[I]) & (flights['DESTINATION_AIRPOR'] == o_path[I+1])]
+    temp_df1 = flights.loc[(flights['ORIGIN_AIRPORT'] == o_path[i]) & (flights['DESTINATION_AIRPORT'] == o_path[i+1])]
     flights_v2 = pd.concat([flights_v2, temp_df1])
 flights_v3 = copy.deepcopy(flights_v2)
 for i in range(0,6,2):
-    temp_df1 = flights.loc[(flights['ORIGIN_AIRPORT'] == d_path[I]) & (flights['DESTINATION_AIRPOR'] == d_path[I+1])]
-    temp_df2 = flights.loc[(flights['ORIGIN_AIRPORT'] == d_path[I+1]) & (flights['DESTINATION_AIRPOR'] == d_path[I])]
+    temp_df1 = flights.loc[(flights['ORIGIN_AIRPORT'] == d_path[i]) & (flights['DESTINATION_AIRPORT'] == d_path[i+1])]
+    temp_df2 = flights.loc[(flights['ORIGIN_AIRPORT'] == d_path[i+1]) & (flights['DESTINATION_AIRPORT'] == d_path[i])]
     temp_df = pd.concat([temp_df1, temp_df2])
     flights_v3 = pd.concat([flights_v3,temp_df])
 
 
 
-date = list(weather['datetim'])
+date = list(weather['datetime'])
 date_v1 = []
 for i in range(len(date)):
     year = int(date[i].split(" ")[0].split("-")[0])
@@ -53,10 +53,10 @@ for i in range(len(date)):
 
 weather = weather.iloc[date_v1]
 weather = weather.reset_index(drop = True)
-weather_v2 = weather[['datetim','Typ','Atlant','Bosto','Denve','Dalla','Miam','New Yor', 'Los Angele'
-,'Minneapoli','Chicag','Seattl','San Francisc']]
+weather_v2 = weather[['datetime','Type','Atlanta','Boston','Denver','Dallas','Miami','New York', 'Los Angeles'
+,'Minneapolis','Chicago','Seattle','San Francisco']]
 
-date1 = list(weather_v2['datetim'])
+date1 = list(weather_v2['datetime'])
 month = []
 day = []
 hour = []
@@ -66,70 +66,71 @@ for i in range(len(weather_v2)):
     day.append(int(date1[i].split(" ")[0].split("-")[2]))
     hour.append(int(date1[i].split(" ")[1].split(":")[0]))
 
-weather_v2['MONT'] = month
-weather_v2['DA'] = day
-weather_v2['HOU'] = hour
+weather_v2['MONTH'] = month
+weather_v2['DAY'] = day
+weather_v2['HOUR'] = hour
 
-weather_v2 = weather_v2.drop(columns = ['datetim'])
+weather_v2 = weather_v2.drop(columns = ['datetime'])
 
-flights_v3 = flights_v3.drop(columns = ['DAY_OF_WEE','YEA','AIR_SYSTEM_DELA',
-       'SECURITY_DELA', 'AIRLINE_DELA', 'LATE_AIRCRAFT_DELA',
-       'WEATHER_DELA','TAIL_NUMBE','FLIGHT_NUMBE','TAXI_OU', 'WHEELS_OF','WHEELS_O','TAXI_I','ELAPSED_TIM','AIR_TIM',
-                                       'DEPARTURE_TIM','ARRIVAL_TIM','DISTANC'])
+flights_v3 = flights_v3.drop(columns = ['DAY_OF_WEEK','YEAR','AIR_SYSTEM_DELAY',
+       'SECURITY_DELAY', 'AIRLINE_DELAY', 'LATE_AIRCRAFT_DELAY',
+       'WEATHER_DELAY','TAIL_NUMBER','FLIGHT_NUMBER','TAXI_OUT', 'WHEELS_OFF','WHEELS_ON','TAXI_IN','ELAPSED_TIME','AIR_TIME',
+                                       'DEPARTURE_TIME','ARRIVAL_TIME','DISTANCE'])
 
-templist = list(flights_v3['SCHEDULED_DEPARTUR'])
+templist = list(flights_v3['SCHEDULED_DEPARTURE'])
 time = []
 
 for i in range(len(flights_v3)):
     value = templist[i]
     time.append((round((value%100) / 60 * 100 + (value//100)* 100, -2))/ 100)
 
-flights_v3['SCHEDULED_DEPARTUR'] = time
-flights_v3 = flights_v3.drop(columns = ['SCHEDULED_TIM','SCHEDULED_ARRIVA','ARRIVAL_DELA'])
-flights_v3 = flights_v3.rename(columns = {'SCHEDULED_DEPARTUR':'HOU'})
+flights_v3['SCHEDULED_DEPARTURE'] = time
+flights_v3 = flights_v3.drop(columns = ['SCHEDULED_TIME','SCHEDULED_ARRIVAL','ARRIVAL_DELAY'])
+flights_v3 = flights_v3.rename(columns = {'SCHEDULED_DEPARTURE':'HOUR'})
 flights_v3 = flights_v3.reset_index(drop = True)
 
-newdf = flights_v3.merge(weather_v2, on = ['MONT','DA','HOU'])
+newdf = flights_v3.merge(weather_v2, on = ['MONTH','DAY','HOUR'])
 
-check = {'AT':'Atlant', 'BO':'Bosto','DE':'Denve','DF':'Dalla',
-       'FL':'Miam', 'JF':'New Yor','LA':'Los Angele','MS':'Minneapoli', 'OR':'Chicag','SE':'Seattl',
-       'SF':'San Francisc'}
+check = {'ATL':'Atlanta', 'BOS':'Boston','DEN':'Denver','DFW':'Dallas',
+       'FLL':'Miami', 'JFK':'New York','LAX':'Los Angeles','MSP':'Minneapolis', 'ORD':'Chicago','SEA':'Seattle',
+       'SFO':'San Francisco'}
 departure = np.unique(newdf['ORIGIN_AIRPORT'])
 comdf = pd.DataFrame([])
 for i in range(len(departure)):
-    tempdf = newdf.loc[newdf['ORIGIN_AIRPORT'] == departure[I]]
-    col = ['Atlant', 'Bosto', 'Denve', 'Dalla',
-       'Miam', 'New Yor', 'Los Angele', 'Minneapoli', 'Chicag', 'Seattl',
-       'San Francisc']
+    tempdf = newdf.loc[newdf['ORIGIN_AIRPORT'] == departure[i]]
+    col = ['Atlanta', 'Boston', 'Denver', 'Dallas',
+       'Miami', 'New York', 'Los Angeles', 'Minneapolis', 'Chicago', 'Seattle',
+       'San Francisco']
     col.remove(check[departure[i]])
     tempdf = tempdf.drop(columns = col)
-    tempdf = tempdf.rename(columns = {check[departure[I]]:'Weathe'})
+    tempdf = tempdf.rename(columns = {check[departure[i]]:'Weather'})
     comdf = pd.concat([comdf,tempdf])
 
 comdf = comdf.reset_index(drop = True)
 comdf.to_csv("/Users/moon/Desktop/commitdf.csv", index = False)
 
+#
 
 def iForest(data, weathers, contamination, flag):
     data = data.loc[(data['Type'] == weathers[0]) | (data['Type'] == weathers[1]) | (data['Type'] == weathers[2])]
     data = data.reset_index(drop=True)
-    dfh = data.loc[data['Type'] == weathers[0]].reset_index(drop=True)
-    dfd = data.loc[data['Type'] == weathers[1]].reset_index(drop=True)
-    dfs = data.loc[data['Type'] == weathers[2]].reset_index(drop=True)
+    df1 = data.loc[data['Type'] == weathers[0]].reset_index(drop=True)
+    df2 = data.loc[data['Type'] == weathers[1]].reset_index(drop=True)
+    df3 = data.loc[data['Type'] == weathers[2]].reset_index(drop=True)
 
-    dfh = dfh.drop(columns=['Type'])
-    dfh = dfh.rename(columns={'Weather': weathers[0]})
-    dfd = dfd.drop(columns=['Type'])
-    dfd = dfd.rename(columns={'Weather': weathers[1]})
-    dfs = dfs.drop(columns=['Type'])
-    dfs = dfs.rename(columns={'Weather': weathers[2]})
+    df1 = df1.drop(columns=['Type'])
+    df1 = df1.rename(columns={'Weather': weathers[0]})
+    df2 = df2.drop(columns=['Type'])
+    df2 = df2.rename(columns={'Weather': weathers[1]})
+    df3 = df3.drop(columns=['Type'])
+    df3 = df3.rename(columns={'Weather': weathers[2]})
 
-    dfh[weathers[1]] = dfd[weathers[1]]
-    dfh[weathers[2]] = dfs[weathers[2]]
+    df1[weathers[1]] = df2[weathers[1]]
+    df1[weathers[2]] = df3[weathers[2]]
 
-    cdf = copy.deepcopy(dfh)
+    cdf = copy.deepcopy(df1)
 
-    cr = list(dfh['CANCELLATION_REASON'])
+    cr = list(df1['CANCELLATION_REASON'])
     crv1 = []
     for i in range(len(cr)):
         if cr[i] == 'B':
@@ -170,7 +171,3 @@ def iForest(data, weathers, contamination, flag):
         plt.show()
     else:
         return cdf
-
-data = pd.read_csv("/Users/moon/Desktop/Business Analytics/Term Project/Data/filtered/commitdf.csv")
-
-iForest(data, ['Humidity','Pressure','Wind_speed'], 0.005, 'graph')
